c8f377e36e919ffb615cab6943709892438cf9bc
Joerg Jungermann <joerg.jungermann@googlemail.com>
2024-04-01 12:40:52 +0200

    example: add to boot.py and main.py

--- boot.py
+++ boot.py
@@ -3,3 +3,16 @@
 # increase CPU speed of Wemos D1/ESP8266
 #from machine import freq
 #freq(160000000)
+
+# clear LED (with 200 LEDs on PIN0) strip on boot, this eats some memory
+#try:
+#    import strip
+#    from machine import Pin
+#    from neopixel import NeoPixel
+#    pin = 0
+#    n = 200
+#    gpio = Pin(pin, Pin.OUT)
+#    np = NeoPixel(gpio, n)
+#    strip.off(np)
+#except Exception as e:
+#    raise(e)
---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py -- put your code here!
+
+#import strip
+
+# moving pixel, white on 15 LEDs
+#strip.wanderingPixel(n = 15)
+#strip.wanderingPixel(n = 15, fg = strip.WHITE)
+# Knight Rider
+#strip.wanderingPixel(n = 15, fg = strip.RED, bg = strip.DARKRED)
+#strip.wanderingPixel(n = 15, fg = strip.BLUE, bg = strip.DARKRED)
+
+# cycling rainbow on 15 LEDs
+#strip.movingPaint(strip.RAINBOW, n = 15, delay_ms = 70)
