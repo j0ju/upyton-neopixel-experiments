0299f53b27d71528369f5010a11d3f55ae752c88
abra <jj@0x2f.org>
2024-08-28 01:12:38 +0200

    GameOfLife2D.py: speed up by using a multidimensional array

--- GameOfLife2D.py
+++ GameOfLife2D.py
@@ -8,9 +8,17 @@ GLIDER = ( ( 1, 1, 1 ),
            ( 0, 1, 0 )
          )
 
+SPACESHIP = ( ( 1, 0, 0, 1, 0),
+              ( 0, 0, 0, 0, 1),
+              ( 1, 0, 0, 0, 1),
+              ( 0, 1, 1, 1, 1)
+            )
+
 # example:
 #   import GameOfLife2D
 #   g = GameOfLife2D.GameOfLife2D(seed=GameOfLife2D.GLIDER)
+#   g = GameOfLife2D.GameOfLife2D(seed=GameOfLife2D.SPACESHIP)
+#   g.run()
 
 class GameOfLife2D:
     # pin = 5 ---> D1 on Wemos D1
@@ -25,85 +33,84 @@ class GameOfLife2D:
 
         gpio = Pin(pin, Pin.OUT)
         self.np = NeoPixel(gpio, n)
-        self.np_swap = NeoPixel(gpio, n)
         strip.off(self.np)
 
         self.DEAD = dead
         self.ALIVE = alive
 
         self.seed(seed)
+        self.display()
     
-        # be aware, we cannot use standard framebuffer calculations here on most WS2812 strips/fields
-        # as they are enumbered as a snake on PCBs
-
-        #  0   1   2   3   4   5  y = 0  ==> y % 2 == 0 ==> cell = py * y   + px
-        # 11  10   9   8   7   6  y = 1  ==> y % 2 == 1 ==> cell = py * y+1 - px - 1
-        # 12  13  14  15  16  17  y = 2
-        # 18  19  20  21  22  23
-    
+    # be aware, we cannot use standard framebuffer calculations here on most WS2812 strips/fields
+    # as they are enumbered as a snake on PCBs
+    #  0   1   2   3   4   5  y = 0  ==> y % 2 == 0 ==> cell = py * y   + px
+    # 11  10   9   8   7   6  y = 1  ==> y % 2 == 1 ==> cell = py * y+1 - px - 1
+    # 12  13  14  15  16  17  y = 2
+    # 18  19  20  21  22  23
     def xy2cell(self, x, y):
         sig = (1, -1)
         return (y+y%2) * self.y + sig[y%2] * (x+y%2)
 
+    # seed world and prepare world array for swaping
     def seed(self, seed = None):
-        if seed == None:
-            from random import getrandbits
-            # TODO: allow seeding of pre-defined data
-            n = self.x * self.y
-            for i in range(0, n):
-                if getrandbits(1) == 1:
-                    self.np[i] = self.ALIVE
-                else:
-                    self.np[i] = self.DEAD
-        else:
-            colormap = (self.DEAD, self.ALIVE)
+        from random import getrandbits
+        self.world = []
+        self.swap = []
+        for y in range(0, self.y):
+            self.world.append([])
+            self.swap.append([])
             for x in range(0, self.x):
-                for y in range(0, self.y):
+                if seed == None:
+                    self.world[y].append(getrandbits(1))
+                else:
                     try:
-                        self.np[self.xy2cell(x,y)] = colormap[seed[x][y]]
+                        self.world[y].append(seed[x][y])
                     except IndexError:
-                        pass
+                        self.world[y].append(0)
+                self.swap[y].append(0)
 
+    def display(self):
+        m = (self.DEAD, self.ALIVE)
+        for y in range(0, self.y):
+            for x in range(0, self.x):
+                self.np[self.xy2cell(x,y)] = m[self.world[y][x]]
         self.np.write()
+
+    def neighbourCount(self, x, y): # pylint: disable=missing-function-docstring
+        count = 0
+        for dx in (-1, 0, 1):
+            px = (x + dx) % self.x
+            for dy in (-1, 0, 1):
+                if dy == 0 and dx == 0:
+                    continue
+                py = (y + dy) % self.y
+                count = count + self.world[py][px]
+        return count
 
     def step(self): # pylint: disable=missing-function-docstring
         for x in range(0, self.x):
             for y in range(0, self.y):
                 neighbours = self.neighbourCount(x,y)
-                cell = self.xy2cell(x, y)
-                if not self.np[cell] == self.DEAD:
+                if self.world[y][x] == 1:
                     # starvation, under population
                     if neighbours < 2:
-                       self.np_swap[cell] = self.DEAD
+                       self.swap[y][x] = 0
                     # over populatiom
                     elif neighbours > 3:
-                        self.np_swap[cell] = self.DEAD
+                       self.swap[y][x] = 0
                     # i am feeling well, and alive
                     else:
-                        self.np_swap[cell] = self.ALIVE
+                       self.swap[y][x] = 1
                 else:
                     if neighbours == 3:
-                        self.np_swap[cell] = self.ALIVE
+                       self.swap[y][x] = 1
                     else:
-                        self.np_swap[cell] = self.DEAD
+                       self.swap[y][x] = 0
 
-        tmp = self.np
-        self.np = self.np_swap
-        self.np_swap = tmp
-        self.np.write()
-
-    def neighbourCount(self, x, y): # pylint: disable=missing-function-docstring
-        count = 0
-        dx = -1
-        for dx in  (-1, 0, 1):
-            px = (x + dx) % self.x
-            for dy in (-1, 0, 1):
-                if dx == 0 and dy == 0:
-                    continue
-                py = (y + dy) % self.y
-                if not self.np[self.xy2cell(px, py)] == self.DEAD:
-                    count = count + 1
-        return count
+        tmp = self.world
+        self.world = self.swap
+        self.swap = tmp
+        self.display()
 
     def run(self, delay=30):          # pylint: disable=missing-function-docstring
         from time import sleep_ms     # pylint: disable=no-name-in-module
