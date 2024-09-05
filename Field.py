996adf90ae0fe3af3a4aeaac62ccf3fc3909df94
abra <jj@0x2f.org>
2024-09-05 18:13:38 +0200

    Field.py: add no-Update() flags to helpers

--- Field.py
+++ Field.py
@@ -25,24 +25,25 @@ class Field:
     # 11  10   9   8   7   6  y = 1  ==> y % 2 == 1 ==> cell = py * y+1 - (px + 1)
     # 12  13  14  15  16  17  y = 2
     # 18  19  20  21  22  23
-    def xy2cell(self, x, y):
+    def xy2n(self, x, y):
         signum = (1, -1)
         return (y+y%2) * self.Y + signum[y%2] * (x+y%2)
 
     def __getitem__(self, k):
-        return self.np[self.xy2cell(k[0],k[1])]
+        return self.np[self.xy2n(k[0],k[1])]
 
     def __setitem__(self, k, v):
-        self.np[self.xy2cell(k[0],k[1])] = v
+        self.np[self.xy2n(k[0],k[1])] = v
 
-    def Wipe(self):
-        strip.off(self.np)
+    def Wipe(self, Update = True):
+        self.Fill(0, 0, self.X, self.Y, strip.BLACK, Update = Update)
 
-    def Fill(self, xmin, ymin, xmax, ymax, color):
+    def Fill(self, xmin, ymin, xmax, ymax, color, Update = True):
         for x in range(xmin, xmax):
             for y in range(ymin, ymax):
                 self[x,y] = color
-        self.Update()
+        if Update:
+            self.Update()
 
     def Update(self):
         self.np.write()
